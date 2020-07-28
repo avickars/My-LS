efd016b440413b1fdfc39ce56fd27937a4faea74
aidan <aidan_vickars@sfu.ca>
2020-07-27 20:03:06 -0700

    quick commit so I can revert if needed

--- directory.c
+++ directory.c
@@ -14,48 +14,52 @@ void read_directory(char *dir, Options *options) {
     struct dirent **namelist;
     int n;
 
-//    n = scandir(".", &namelist, NULL, NULL);
-//    while (n--) {
+    n = scandir(".", &namelist, NULL, NULL);
+    while (n--) {
 //        printf("%s\n", namelist[n]->d_name);
-//        free(namelist[n]);
-//    }
-//    free(namelist);
+        print(namelist[n], options);
+        free(namelist[n]);
+    }
+    free(namelist);
 //
+
+
+
 //    char subDirectory[SIZEOFSUBDIRECTORY];
 //    DIR *directory;
 //    struct dirent *de;
 //    if ((directory = opendir (dir)) == NULL) {
 //        return;
 //    }
-
-    while ((de = readdir(directory)) != NULL) {
-        // Removing the current directory form output
-        if (strcmp(de->d_name, ".") == 0) {
-            continue;
-        }
-
-        // Removing the parent directory from output
-        if (strcmp(de->d_name, "..") == 0) {
-            continue;
-        }
-
-        // Removing hidden files from output
-        if (de->d_name[0]=='.') {
-            continue;
-        }
-        strcpy(subDirectory, dir);
-        strcat(subDirectory,"/");
-        strcat(subDirectory,de->d_name);
-
-        // Passing to a print routine
-        print(subDirectory, options);
-
-        // If user asks to recursively print the names
-        if (options->R) {
-            // CITATION: https://www.programiz.com/c-programming/examples/lexicographical-order
-            read_directory(subDirectory, options); // Recursively enters read_directory again
-        }
-    }
-    closedir(directory);
+//
+//    while ((de = readdir(directory)) != NULL) {
+//        // Removing the current directory form output
+//        if (strcmp(de->d_name, ".") == 0) {
+//            continue;
+//        }
+//
+//        // Removing the parent directory from output
+//        if (strcmp(de->d_name, "..") == 0) {
+//            continue;
+//        }
+//
+//        // Removing hidden files from output
+//        if (de->d_name[0]=='.') {
+//            continue;
+//        }
+//        strcpy(subDirectory, dir);
+//        strcat(subDirectory,"/");
+//        strcat(subDirectory,de->d_name);
+//
+//        // Passing to a print routine
+//        print(subDirectory, options);
+//
+//        // If user asks to recursively print the names
+//        if (options->R) {
+//            // CITATION: https://www.programiz.com/c-programming/examples/lexicographical-order
+//            read_directory(subDirectory, options); // Recursively enters read_directory again
+//        }
+//    }
+//    closedir(directory);
 }
 
