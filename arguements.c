36a4eea912638d4b5d9ddc9a5c9a7f23735fe3c6
aidan <aidan_vickars@sfu.ca>
2020-08-09 13:37:28 -0700

    added in error handler on options

--- arguements.c
+++ arguements.c
@@ -13,14 +13,12 @@ int arguements_handler(Options *options, int numArgs,char *args[]) {
             for (int j = 1; j < strlen(args[i]); ++j) {
                 if (args[i][j] == 'i') {
                     options->i = true;
-                }
-
-                if (args[i][j] == 'l') {
+                } else if (args[i][j] == 'l') {
                     options->l = true;
-                }
-
-                if (args[i][j] == 'R') {
+                } else if (args[i][j] == 'R') {
                     options->R = true;
+                } else {
+                    printf("Invalid Option: %c\n", args[i][j]);
                 }
             }
         } else {
